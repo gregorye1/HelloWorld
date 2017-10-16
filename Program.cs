509e638f2ac81913a726fece81343797634033fa
gregorye1 <32143199+gregorye1@users.noreply.github.com>
2017-10-16 13:01:36 -0600

    Hello World file

--- Program.cs
+++ Program.cs
@@ -6,8 +6,98 @@ namespace HelloWorld
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello");
+            Start();
+        }
+
+        public static void Start()
+        {
+            Console.WriteLine("Write 2 numbers");
+            float number1 = float.Parse(Console.ReadLine());
+			float number2 = float.Parse(Console.ReadLine());
+
+			Console.WriteLine("Select an option\n1.-Add\n2.-Subtract\n3.-Multiply\n4.-Divide\n5.-Exit");
+			int option = int.Parse(Console.ReadLine());
+
+			EvaluateOption(option, number1, number2); 
+
+        }
+
+        public static float AddTwoNumbers(float n1, float n2)
+        {
+            float result = n1 + n2;
+
+            return result;
+        }
+
+		public static float SubtractTwoNumbers(float n1, float n2)
+		{
+			float result = n1 - n2;
+
+			return result;
+		}
+
+		public static float MultiplyTwoNumbers(float n1, float n2)
+		{
+			float result = n1 * n2;
+
+			return result;
+		}
+
+		public static float DivideTwoNumbers(float n1, float n2)
+		{
+			float result = n1 / n2;
+
+			return result;
+		}
+        public static void EvaluateOption(int option, float n1, float n2)
+        {
+            /*if(option == 1)
+            {
+                Console.WriteLine("Result of the addition: " + AddTwoNumbers(n1, n2));
+			}
+            else if(option == 2)
+            {
+                Console.WriteLine("Result of the subtraction: " + SubtractTwoNumbers(n1, n2));
+			}
+			else if (option == 3)
+			{
+				Console.WriteLine("Result of the multiplication: " + MultiplyTwoNumbers(n1, n2));
+			}
+			else if (option == 4)
+			{
+				Console.WriteLine("Result of the division: " + DivideTwoNumbers(n1, n2));
+			}
+            else if (option == 5)
+            {
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Invalid option");  
+            }*/
+
+            switch(option)
+            {
+                case 1:
+                    Console.WriteLine("Result of the addition: " + AddTwoNumbers(n1, n2));
+                    break;
+                case 2:
+                    Console.WriteLine("Result of the subtraction: " + SubtractTwoNumbers(n1, n2));
+                    break;
+                case 3:
+					Console.WriteLine("Result of the multiplication: " + MultiplyTwoNumbers(n1, n2));
+                    break;
+                case 4:
+					Console.WriteLine("Result of the division: " + DivideTwoNumbers(n1, n2));
+                    break;
+                case 5:
+                    return;
+                default:
+					Console.WriteLine("Invalid option");
+					break;
+			}
+
+            Start();
         }
     }
 }
